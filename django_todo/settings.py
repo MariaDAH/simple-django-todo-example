1f4ce473edd524889f77f4ed3f648a69407dfd8d
EC2 Default User <ec2-user@ip-172-31-29-83.eu-west-1.compute.internal>
2018-12-10 12:15:40 +0000

    Change title on templates

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,15 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'p%gek33y9#_u41d$*tx%1#gr=-^ok_2&!7yzb*jm0valyw6r01'
-SECRET_KEY = os.environ.get('SECRET_KEY','p%gek33y9#_u41d$*tx%1#gr=-^ok_2&!7yzb*jm0valyw6r01')
+SECRET_KEY = 'p%gek33y9#_u41d$*tx%1#gr=-^ok_2&!7yzb*jm0valyw6r01'
+#SECRET_KEY = os.environ.get('SECRET_KEY','p%gek33y9#_u41d$*tx%1#gr=-^ok_2&!7yzb*jm0valyw6r01')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['b2964d7d76fd4ddfb0c14b5f8e51e754.vfs.cloud9.eu-west-1.amazonaws.com', 'simple-django-todo-example.herokuapp.com']
-AllOWED_HOST = [os.environ.get('C9_HOSTNAME'),os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = ['b2964d7d76fd4ddfb0c14b5f8e51e754.vfs.cloud9.eu-west-1.amazonaws.com', 'simple-django-todo-example.herokuapp.com']
+#AllOWED_HOST = [os.environ.get('C9_HOSTNAME'),os.environ.get('HOSTNAME')]
 
 
 
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-#DATABASES = {'default': dj_database_url.parse("postgres://zzgfjgxyyyzakk:45a45deba886cdf589023797e376c07e46bf017eda4ca592473310b7a3c51f39@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/d8uj9ogq8nf9ug")}
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.parse("postgres://zzgfjgxyyyzakk:45a45deba886cdf589023797e376c07e46bf017eda4ca592473310b7a3c51f39@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/d8uj9ogq8nf9ug")}
+#DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
